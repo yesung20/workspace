f3311a036e1d470f865ec09463806e027d534378
yesung20 <155213489+yesung20@users.noreply.github.com>
2024-02-12 03:18:43 +0900

    Create Article.java

---
+++ project_board/Article.java
@@ -0,0 +1,41 @@
+package main.borad;
+
+public class Article {
+    //게시판에 있는 글 하나에 들어갈 내용
+    private int articleNum;
+    private String title;
+    private String content;
+    private String userId;
+
+    public int getArticleNum() {
+        return articleNum;
+    }
+
+    public void setArticleNum(int articleNum) {
+        this.articleNum = articleNum;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
